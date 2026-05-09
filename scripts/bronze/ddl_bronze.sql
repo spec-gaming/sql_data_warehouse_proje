a2a7e0cccfaacdda21ebd312261bebb9d97b63e0
spec-gaming <113355121+spec-gaming@users.noreply.github.com>
2026-05-09 08:55:29 +0100

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -5,7 +5,7 @@ cst_id INT,
 cst_key VARCHAR(50),
 cst_firstname VARCHAR(50),
 cst_lastname VARCHAR(50),
-cst_material_status VARCHAR(50),
+cst_marital_status VARCHAR(50),
 cst_gndr VARCHAR(50),
 cst_create_date DATE
 );
